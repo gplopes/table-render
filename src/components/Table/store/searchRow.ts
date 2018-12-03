726e24aa47af4ec752469d0204c1dc95e5134180
Gabriel <lenkapavlikova@Lenkas-MacBook-Pro.local>
2018-12-03 21:08:03 +0100

    first interaction redux

--- src/components/Table/store/searchRow.ts
+++ src/components/Table/store/searchRow.ts
@@ -1,30 +1,27 @@
 import isObject from "lodash/isObject";
 
 let itemFound = false;
+let newTable = [];
 
 export function searchDeepRow(table: any, id: string) {
-  if (itemFound) return false;
-  console.log("table", typeof table);
+  if (itemFound) {
+    console.log('item found -->', itemFound);
+    return false;
+  }
   if (table instanceof Array) {
     table.forEach(item => {
       if (item._id === id) {
-        console.log("Hey! found it");
         itemFound = true;
-      }
+      } else {
       searchDeepRow(item, id);
+      }
     });
   } else if (isObject(table)) {
     for (let prop of Object.keys(table)) {
       if (typeof table[prop] !== "string") {
-        //console.log("prop", prop);
         searchDeepRow(table[prop], id);
       }
     }
-  }
-
-  if (table === null) {
-    console.log("hey done");
-    return false;
   }
 }
 
